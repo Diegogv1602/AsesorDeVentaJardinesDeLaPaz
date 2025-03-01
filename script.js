1c489ce972e018c4fec7633974afa2c46d6ce087
Diego <degoadolfo945@gmail.com>
2025-03-01 18:13:37 -0500

    Primer commit - Agregando archivos del proyecto

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Bienvenido a mi página web");
+});
+document.getElementById("miBoton").addEventListener("click", function() {
+    alert("¡Bienvenido a mi página web!");
+});
